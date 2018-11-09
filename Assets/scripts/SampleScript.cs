4f1c7770d47b364d30f9b293899cb743a55c0a2c
Kostia Yeriomin <adaw36909@gmail.com>
2018-11-09 14:56:26 +0200

    innit commit

---
+++ Assets/scripts/SampleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour {
+
+	private Soft Soft;
+	
+	
+	void Start () {
+		Debug.Log("START");
+		Soft = new Soft();
+		Soft.StartClient();
+	}
+	
+	void Update () {
+		
+	}
+}
